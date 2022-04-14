a155c46afa0ec83f0639062064488c8a3dc7f386
Wardshan Essam <rose.essam38@gmail.com>
2022-04-14 20:46:38 +0200

    added posts view

--- client/memoriesapp/src/components/Posts/Post/Post.js
+++ client/memoriesapp/src/components/Posts/Post/Post.js
@@ -1,6 +1,44 @@
-const Post = ()=>{
+import {Card,CardActions,CardContent,CardMedia,Button,Typography} from '@mui/material';
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt'
+import DeleteIcon from '@mui/icons-material/Delete'
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz'
+import moment from 'moment';
+const Post = ({post})=>{
     return(
-        <h1>Post</h1>
+       <Card>
+           <CardMedia
+           height="194"
+           component="img"
+           image = {post.selectedFile}
+           title= {post.title} />
+           <div>
+               <Typography variant="h6">{post.creator}</Typography>
+               <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+           </div>
+           <div>
+               <Button style={{color:'black'}} size="small" onClick={()=>{}}>
+                   <MoreHorizIcon fontSize='default'/>
+               </Button>
+           </div>
+           <div>
+           <Typography variant="body2" color='primary'>{post.tags.map((tag)=>`# ${tag} `)}</Typography>
+          
+           </div>
+           <CardContent>
+           <Typography variant="h5" gutterBottom>{post.message}</Typography>
+           </CardContent>
+           <CardActions>
+               <Button size='small' color='primary' onClick={()=>{}}>
+                   <ThumbUpAltIcon fontSize='small'/>
+                   Like
+                   {post.likeCount}
+               </Button>
+               <Button size='small' color='primary' onClick={()=>{}}>
+                   <DeleteIcon fontSize='small'/>
+                   Delete
+               </Button>
+           </CardActions>
+       </Card>
     )
 }
 export default Post; 
--- client/memoriesapp/src/components/Posts/Posts.js
+++ client/memoriesapp/src/components/Posts/Posts.js
@@ -1,14 +1,21 @@
 import Post from './Post/Post'
 import { useSelector } from 'react-redux';
+import {Grid,CircularProgress} from '@mui/material'
 const Posts = ()=>{
     const posts = useSelector((state)=> state.posts);
     console.log(posts);
     return(
-        <>
-        <h1>Posts</h1>
-        <Post/>
-        <Post/>
-        </>
+       !posts.length ? <CircularProgress /> : (
+           <Grid alignItems='stretch'>
+              {
+                 posts.map((post)=>(
+                     <Grid item key={post._id} xs={12} sm={6}>
+                            <Post post={post}/>
+                     </Grid>
+                 ))
+              }
+           </Grid>
+       )
     )
 }
 export default Posts; 
